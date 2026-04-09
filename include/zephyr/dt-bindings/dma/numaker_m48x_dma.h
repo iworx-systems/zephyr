8dc1bb07a464e2f6d37761dac8a6406158016f51
Scott Laboe <scottyl2.718@gmail.com>
2026-04-09 19:43:30 +0000

    drivers: dma: numaker: dt-bindings
     - Add dma header with defintions used in the devicetree.
     - M480 soc disabled pdma by default.

---
+++ include/zephyr/dt-bindings/dma/numaker_m48x_dma.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2026 iWorx Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_DMA_NUMAKER_M48X_DMA_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_DMA_NUMAKER_M48X_DMA_H_
+
+/* PDMA request source slot numbers for M48x series */
+#define NUMAKER_PDMA_MEM            0
+#define NUMAKER_PDMA_USB_TX         2
+#define NUMAKER_PDMA_USB_RX         3
+#define NUMAKER_PDMA_UART0_TX       4
+#define NUMAKER_PDMA_UART0_RX       5
+#define NUMAKER_PDMA_UART1_TX       6
+#define NUMAKER_PDMA_UART1_RX       7
+#define NUMAKER_PDMA_UART2_TX       8
+#define NUMAKER_PDMA_UART2_RX       9
+#define NUMAKER_PDMA_UART3_TX       10
+#define NUMAKER_PDMA_UART3_RX       11
+#define NUMAKER_PDMA_UART4_TX       12
+#define NUMAKER_PDMA_UART4_RX       13
+#define NUMAKER_PDMA_UART5_TX       14
+#define NUMAKER_PDMA_UART5_RX       15
+#define NUMAKER_PDMA_USCI0_TX       16
+#define NUMAKER_PDMA_USCI0_RX       17
+#define NUMAKER_PDMA_USCI1_TX       18
+#define NUMAKER_PDMA_USCI1_RX       19
+#define NUMAKER_PDMA_QSPI0_TX       20
+#define NUMAKER_PDMA_QSPI0_RX       21
+#define NUMAKER_PDMA_SPI0_TX        22
+#define NUMAKER_PDMA_SPI0_RX        23
+#define NUMAKER_PDMA_SPI1_TX        24
+#define NUMAKER_PDMA_SPI1_RX        25
+#define NUMAKER_PDMA_SPI2_TX        26
+#define NUMAKER_PDMA_SPI2_RX        27
+#define NUMAKER_PDMA_SPI3_TX        28
+#define NUMAKER_PDMA_SPI3_RX        29
+#define NUMAKER_PDMA_QSPI1_TX       30
+#define NUMAKER_PDMA_QSPI1_RX       31
+#define NUMAKER_PDMA_EPWM0_P1_RX    32
+#define NUMAKER_PDMA_EPWM0_P2_RX    33
+#define NUMAKER_PDMA_EPWM0_P3_RX    34
+#define NUMAKER_PDMA_EPWM1_P1_RX    35
+#define NUMAKER_PDMA_EPWM1_P2_RX    36
+#define NUMAKER_PDMA_EPWM1_P3_RX    37
+#define NUMAKER_PDMA_I2C0_TX        38
+#define NUMAKER_PDMA_I2C0_RX        39
+#define NUMAKER_PDMA_I2C1_TX        40
+#define NUMAKER_PDMA_I2C1_RX        41
+#define NUMAKER_PDMA_I2C2_TX        42
+#define NUMAKER_PDMA_I2C2_RX        43
+#define NUMAKER_PDMA_I2S0_TX        44
+#define NUMAKER_PDMA_I2S0_RX        45
+#define NUMAKER_PDMA_TMR0           46
+#define NUMAKER_PDMA_TMR1           47
+#define NUMAKER_PDMA_TMR2           48
+#define NUMAKER_PDMA_TMR3           49
+#define NUMAKER_PDMA_EADC0_RX       50
+#define NUMAKER_PDMA_DAC0_TX        51
+#define NUMAKER_PDMA_DAC1_TX        52
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_DMA_NUMAKER_M48X_DMA_H_ */
